7b940cf0dbf8ab2d9b2a1804aa9de6f5ad585f48
stymwg <94900556+stymwg@users.noreply.github.com>
2022-02-15 22:53:30 +0000

    Add files via upload

---
+++ SR_S7_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=S7_Unicycler 
+#SBATCH --partition=hpc 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=15g
+#SBATCH --time=24:00:00
+#SBATCH --output=/shared/Project1_Resources/GROUP2_Workspace/%x.out
+#SBATCH --error=/shared/Project1_Resources/GROUP2_Workspace/%x.err
+
+
+source $HOME/.bash_profile
+conda activate /shared/home/mbxlw2/miniconda3/envs/unicyclerENV
+ 
+unicycler -1 /shared/home/mbxlw2/Unicycler/S8/H5303_S7_ALL_R1_001.fastq.gz -2 /shared/home/mbxlw2/Unicycler/S8/H5303_S7_ALL_R2_001.fastq.gz -o /shared/Project1_Resources/GROUP2_Workspace/H5303_S7_ALL
