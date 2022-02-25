4b3db25f831214256bc7e7cdc83022b7fcf45ca6
stymwg <94900556+stymwg@users.noreply.github.com>
2022-02-25 00:09:56 +0000

    Add files via upload

---
+++ passfail_assembly_LR_S1.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=S1_pass_fail_flye
+#SBATCH --partition=hpc
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=15g
+#SBATCH --time=05:00:00
+#SBATCH --output=/shared/Project1_Resources/GROUP2_Workspace/lewis_flye/barcode_1_fastq_fail/slurm.out
+#SBATCH --error=/shared/Project1_Resources/GROUP2_Workspace/lewis_flye/barcode_1_fastq_fail/slurm.err
+
+source $HOME/.bash_profile
+conda activate /shared/home/mbxlw2/miniconda3/envs/unicyclerENV/envs/flye
+
+python /shared/home/mbxlw2/Flye/bin/flye --nano-hq /shared/home/mbxlw2/Flye/pass_data/barcode01/* /shared/home/mbxlw2/Flye/fail_data/barcode01/* --out-dir /shared/Project1_Resources/GROUP2_Workspace/lewis_flye/barcode_1_fastq_fail
